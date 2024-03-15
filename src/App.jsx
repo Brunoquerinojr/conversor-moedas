1d2e92ad10b2be2640f8d6fba6ac6d03eed0fafb
Brunoquerinojr <brunoquerinosilvajr@gmail.com>
2024-03-15 16:09:41 -0300

    moedasfim

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import CurrencyConverter from './components/CurrencyConverter'
 
 function App() {
 
-
+  document.body.style.backgroundColor = '#434b44'; 
   return (
     <>
       <CurrencyConverter />
